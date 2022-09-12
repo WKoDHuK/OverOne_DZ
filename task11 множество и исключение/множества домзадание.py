e45f97e4d708ab39b8cad51fd2ad3051e704e2d0
alex <alexdonukr@gmail.com>
2022-09-12 12:19:07 +0300

    это пробный commet

--- task11 множество и исключение/множества домзадание.py
+++ task11 множество и исключение/множества домзадание.py
@@ -7,13 +7,13 @@ except ZeroDivisionError:
     print(int(input('Введите заново число')))
 
 print(c)
-
-try:
-   num_3 = num_1/num_2       # делим чсило на 0
-except ZeroDivisionError:    # видит ошибку деления на ноль
-    num_3=0                  # ставим что результат равен 0
-    print("Что то пошло не так")
-else:                        # если нормально все, то возведет в квадрат
-    print(num_3**2)
-finally:
-    print("Задача выполнена")
+#
+# try:
+#    num_3 = num_1/num_2       # делим чсило на 0
+# except ZeroDivisionError:    # видит ошибку деления на ноль
+#     num_3=0                  # ставим что результат равен 0
+#     print("Что то пошло не так")
+# else:                        # если нормально все, то возведет в квадрат
+#     print(num_3**2)
+# finally:
+#     print("Задача выполнена")
